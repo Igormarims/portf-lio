ea4567e3baebf8f62c14cb4dfa8b42853b7088c3
Igor marques <igormarquespe@gmail.com>
2022-11-30 04:34:58 -0300

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './Header.styles.css'
+import CTA from './CTA'
+import Me from '../../assets/img1.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+     <div className="container header__container">
+        <h5>Hello</h5>
+        <h1>Igor Marques</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+        <HeaderSocials/>
+        <div className="me">
+          <img className='me__photo' src={ Me } alt="" />
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+   </header> 
   )
 }
 
